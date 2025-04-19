f8ee7fa27cc5a900fc2640482d14a2dfc601efb3
kiaerii <kakabk35@gmail.com>
2025-04-19 11:29:14 +0300

    Update задание 4.py

--- задание 4.py
+++ задание 4.py
@@ -5,12 +5,12 @@ class BankAccount:
         self._transactions = []  #список для хранения истории операций
     
     def deposit(self, amount): #Пополнение счета на указанную сумму
-        if amount <= 0:
+        if amount <= 0: #сумма, на котор. надо пополнить
             raise ValueError('Сумма пополнения должна быть положительной')
         self._balance += amount #кол-во 
         self._transactions.append(f'Пополнение: +{amount}')
     
-    def withdraw(self, amount): #Снятие средств со счета
+    def withdraw(self, amount): #Снятие средств со счета (безопасное)
         if amount <= 0: #кол-во денег 
             raise ValueError('Сумма снятия должна быть положительной')
         if amount > self._balance:
@@ -18,7 +18,7 @@ class BankAccount:
         self._balance -= amount
         self._transactions.append(f'Снятие: -{amount}')
     
-    @property
+    @property #обращение к балансу как к атрибуту
     def balance(self): #Геттер для получения текущего баланса
         return self._balance
     
